0cc2a8c04d18b761fbeb132318095982ec151864
Scyano <scyano@icloud.com>
2018-01-26 17:50:23 +0800

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Scyano on 26/01/2018.
+//  Copyright © 2018 Scyano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
